6056e9dbd62aaab9a0a6b809a9de7b5fe1fd5fe0
安東芙実 <fokaron3428hatimitu@gmail.com>
2017-04-10 21:05:57 +0900

    並列の内容の変更
    配列の記入の変更と逆順に表示できるように変更しました。

--- Assets/ScriptTest.cs
+++ Assets/ScriptTest.cs
@@ -32,10 +32,8 @@ public class ScriptTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int[] array = {1,2,3,4,5};
-		for (int i = 0; i < array.Length; i++) {
-			Debug.Log (array [i]);
-		}
+		
+
 
 		Boss lastboss = new Boss ();
 		lastboss.Attack();
@@ -43,6 +41,19 @@ public class ScriptTest : MonoBehaviour {
 		for (int i = 0; i < 11; i++) {
 			lastboss.Magic();
 		}
+		int[] array = new int[5];
+		array [0] = 1;
+		array [1] = 2;
+		array [2] = 3;
+		array [3] = 4;
+		array [4] = 5;
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (array [i]);
+		}
+		for (int i = 4; i > -1; i--) {
+			Debug.Log (array [i]);
+		}
+
 	}
 	
 	// Update is called once per frame
